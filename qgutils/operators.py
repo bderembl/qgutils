85462f95cb90ae5d8a948f9241ac99b731bfc350
bderembl <bruno.deremble@ens.fr>
2021-03-19 12:27:14 +0100

    add curl

--- qgutils/operators.py
+++ qgutils/operators.py
@@ -76,3 +76,37 @@ def interp_on_b(p, dh):
 
   # weighted average (cf. Holland 1978)
   return (p[:-1,:,:]*dh[1:,None,None] + p[1:,:,:]*dh[:-1,None,None])/(dh[:-1,None,None] + dh[1:,None,None])
+
+
+def curl(u, v, Delta, mode):
+  """
+  Compute curl of a vector field.
+
+  dv/dx - dudy
+  
+  This operation depends on the position of the vector field. 
+
+  mode = v2c: u and v are defined on vertices and output is at cell center
+
+  Parameters
+  ----------
+
+  u : array [(nz,)ny,nx]  (size of u and v may vary)
+  v : array [(nz,)ny,nx]
+  Delta: float
+  mode: str, see description
+
+  Returns
+  -------
+
+  curluv: array [(nz,) ny,nx] (horizontal dimension may vary)
+  """
+
+  if mode == "v2c":
+
+    curluv = 0.5*(v[...,1:,1:] - v[...,1:,:-1]
+                  + v[...,:-1,1:] - v[...,:-1,:-1]
+                  - u[...,1:,1:]  + u[...,:-1,1:]
+                  - u[...,1:,:-1] + u[...,:-1,:-1])/Delta
+
+  return curluv
