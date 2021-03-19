2778d65a82172ba58534b34bcb59ddaf14459ad4
bderembl <bruno.deremble@ens.fr>
2021-03-19 12:30:42 +0100

    lorenz cycle with buoyancy forcing

--- qgutils/energy.py
+++ qgutils/energy.py
@@ -131,17 +131,21 @@ def comp_pe(psi, dh,N2,f0,Delta):
   return pe
 
 
-def integral_plev(psi, dh, Delta, average=False):
+def integral(psi, dh, Delta, average=False):
 
   '''
-  Compute integral of a field defined at a p-level
+  Compute integral of a field
+
+  if the field is defined on p-levels, the integral is the usual integral
+  if the field is defined on b-levels, we assume that psi = 0 at
+  the upper and lower boundaries
 
 
   Parameters
   ----------
 
-  psi : array [nz, ny,nx]
-  dh : array [nz]
+  psi : array [nz, ny,nx] for p-level field or array [nz-1, ny,nx] for b-level field
+  dh : array [nz], *dh is the layer thickness of the p-level* (no matter what)
   Delta: float
   average: if True: divide the integral by the total volume (default is False)
 
@@ -152,55 +156,26 @@ def integral_plev(psi, dh, Delta, average=False):
 
   '''
   si = psi.shape
+  nl0 = si[0]
   N = si[-1]
+  nl = len(dh)
 
+  # total H even for b-levels
   Ht = np.sum(dh)
 
+  dhl = np.copy(dh)
+  if nl0 == nl-1:
+    dhl = 0.5*(dh[1:] + dh[:-1])
+
   if average:
-    psi_i = np.sum(psi*dh[:,None,None])/Ht/N**2
+    psi_i = np.sum(psi*dhl[:,None,None])/Ht/N**2
   else:
-    psi_i = np.sum(psi*dh[:,None,None])*Delta*Delta
+    psi_i = np.sum(psi*dhl[:,None,None])*Delta*Delta
 
   return psi_i
 
 
-def integral_blev(psi, dh, Delta, average=False):
-
-  '''
-  Compute integral of a field defined at a b-level
-
-
-  Parameters
-  ----------
-
-  psi : array [nz-1, ny,nx]
-  dh : array [nz] **dh is the layer thickness of the p-level**
-  Delta: float
-  average: if True: divide the integral by the total volume (default is False)
-
-  Returns
-  -------
-
-  psi_i = scalar
-
-  '''
-
-  si = psi.shape
-  N = si[-1]
-
-  dhi = 0.5*(dh[1:] + dh[:-1])
-  Ht = np.sum(dh) # we still divide by the total thickness (assume b=0 at top and
-                  # bottom)
-
-  if average:
-    psi_i = np.sum(psi*dhi[:,None,None])/Ht/N**2
-  else:
-    psi_i = np.sum(psi*dhi[:,None,None])*Delta*Delta
-
-  return psi_i
-
-
-def lorenz_cycle(pfiles,dh,N2,f0,Delta,bf=0, nu=0, nu4=0, forcing=0):
+def lorenz_cycle(pfiles,dh,N2,f0,Delta,bf=0, nu=0, nu4=0, forcing_z=0, forcing_b=0, toc=0, nu_in_b=True, average=False):
   '''
   Compute Lorenz energy cycle
 
@@ -216,7 +191,15 @@ def lorenz_cycle(pfiles,dh,N2,f0,Delta,bf=0, nu=0, nu4=0, forcing=0):
   of the bottom Ekman layer, or bf = Ekb/(Rom*2*dh[-1]) with non dimensional params)
   nu: scalar, harmonic viscosity
   nu4: scalar, bi-harmonic viscosity
-  forcing : array [ny,nx] (exactly the same as the rhs of the PV eq.)
+  forcing_z : array [ny,nx] wind forcing (exactly the same as the rhs of the PV eq.)
+              or list of files
+  forcing_b : array [ny,nx]  =(buoyancy forcing)/N2 (entoc), or list of files
+              if list of files, load sst and you need to provide toc
+  toc :  array[nz] temperature anomaly (only used for buoyancy forcing)
+  nu_in_b : Bool,
+            if QG equations are derived from PE there is dissip in PE (nu_in_b=True)        
+            if QG equations are derived from SW there is no thichness dissipation (nu_in_b=False)
+  average : Bool, if False, return energy integral (default), if True: return energy average
 
   Returns
   -------
@@ -225,29 +208,48 @@ def lorenz_cycle(pfiles,dh,N2,f0,Delta,bf=0, nu=0, nu4=0, forcing=0):
     e.g. if mke2mpe >0 then there is a transfer from mke to mpe
   '''
 
-  nf  = len(pfiles)
+
   N2,f0 = reshape3d(dh,N2,f0)
 
-  p = np.fromfile(pfiles[0],'f4')
-  N = int(p[0])
-  N1 = N + 1
-  nl = int(len(p)/N1**2)
+  si_t = read_time(pfiles)
+
+  p = load_generic(pfiles, 0, 'p', 1/f0, interp=True)
+  nl,N,naux = p.shape
+  
+  if not isinstance(forcing_z, list):
+    loc_forcing_z = np.copy(forcing_z)
+
+  if not isinstance(forcing_b, list):
+    loc_forcing_b = np.copy(forcing_b)
   
   # compute mean
   p_me = np.zeros((nl,N,N))
   w_me = np.zeros((nl-1,N,N))
+  f_me = np.zeros((N,N))
+  d_me = np.zeros((N,N))
 
-  for ifi in range(0,nf):
+  for it in range(0,si_t):
   
-    p = read_bas(pfiles[ifi])
-    w = get_w(p,dh, N2[:,0,0],f0[0,0], Delta, bf,forcing)
+    p = load_generic(pfiles, it, 'p', rescale=1/f0, interp=True, si_t=si_t, subtract_bc=True)
+    if isinstance(forcing_z, list):
+      loc_forcing_z = load_generic(forcing_z, it, 'wekt', rescale=f0/dh[0], interp=False, si_t=si_t)
+    if isinstance(forcing_b, list):
+      sst = load_generic(forcing_b, it, 'sst', interp=False, si_t=si_t)
+      wekt = loc_forcing_z*dh[0]/f0 # remove scaling
+      entoc = -0.5*wekt*( sst - toc[0] ) /(toc[0]-toc[1])
+      loc_forcing_b = entoc - np.mean(entoc)
+
+    w = get_w(p,dh, N2[:,0,0],f0[0,0], Delta, bf,loc_forcing_z, loc_forcing_b, nu=(not nu_in_b)*nu, nu4=(not nu_in_b)*nu4)
   
     p_me += p
     w_me += w
+    f_me += loc_forcing_z
+    d_me += loc_forcing_b
   
-  
-  p_me /= nf
-  w_me /= nf
+  p_me /= si_t
+  w_me /= si_t
+  f_me /= si_t
+  d_me /= si_t
   
   z_me = laplacian(p_me,Delta)
   b_me = p2b(p_me, dh, f0)
@@ -256,11 +258,12 @@ def lorenz_cycle(pfiles,dh,N2,f0,Delta,bf=0, nu=0, nu4=0, forcing=0):
   ke_me = comp_ke(p_me,Delta)
   pe_me = comp_pe(p_me, dh, N2,f0, Delta)
   
-  ei_ke_me = integral_plev(ke_me, dh, Delta)
-  ei_pe_me = integral_blev(pe_me, dh, Delta)
+  ei_ke_me = integral(ke_me, dh, Delta, average)
+  ei_pe_me = integral(pe_me, dh, Delta, average)
   
   e_surf   = np.zeros((nl,N,N))
   e_bottom = np.zeros((nl,N,N))
+  e_diab   = np.zeros((nl-1,N,N))
   
   dissip_k_me = -nu4*laplacian(laplacian(z_me,Delta),Delta)
   dissip_p_me = -nu4*laplacian(laplacian(s_me,Delta),Delta)
@@ -270,34 +273,45 @@ def lorenz_cycle(pfiles,dh,N2,f0,Delta,bf=0, nu=0, nu4=0, forcing=0):
   
   bottom_ekman = -bf*laplacian(p_me[-1,:,:],Delta)
   
-  e_surf[0,:,:] = -p_me[0,:,:]*forcing
+  e_surf[0,:,:] = -p_me[0,:,:]*f_me
   e_bottom[-1,:,:] = -p_me[-1,:,:]*bottom_ekman
   
-  ei_surf_me   = integral_plev(e_surf, dh, Delta)
-  ei_bottom_me = integral_plev(e_bottom, dh, Delta)
-  ei_diss_k_me = integral_plev(-p_me*dissip_k_me, dh, Delta)
-  ei_diss_p_me = integral_plev(-p_me*dissip_p_me, dh, Delta)
-  ei_wb_me     = integral_blev(w_me*b_me, dh, Delta)
+  e_diab[0,:,:] = b_me[0,:,:]*d_me
+
+  ei_surf_me   = integral(e_surf, dh, Delta, average)
+  ei_bottom_me = integral(e_bottom, dh, Delta, average)
+  ei_diss_k_me = integral(-p_me*dissip_k_me, dh, Delta, average)
+  ei_diss_p_me = integral(-p_me*dissip_p_me, dh, Delta, average)
+  ei_wb_me     = integral(w_me*b_me, dh, Delta, average)
+  ei_diab_me   = integral(e_diab, dh, Delta, average)
   
   # compute all terms
-  ei_ke   = np.zeros(nf)
-  ei_pe   = np.zeros(nf)
-  ei_surf   = np.zeros(nf)
-  ei_bottom = np.zeros(nf)
-  ei_diss_k = np.zeros(nf)
-  ei_diss_p = np.zeros(nf)
-  ei_wb     = np.zeros(nf)
-  ei_ke_me2ke_p = np.zeros(nf)
-  ei_pe_me2pe_p = np.zeros(nf)
+  ei_ke   = np.zeros(si_t)
+  ei_pe   = np.zeros(si_t)
+  ei_surf   = np.zeros(si_t)
+  ei_bottom = np.zeros(si_t)
+  ei_diss_k = np.zeros(si_t)
+  ei_diss_p = np.zeros(si_t)
+  ei_wb     = np.zeros(si_t)
+  ei_ke_me2ke_p = np.zeros(si_t)
+  ei_pe_me2pe_p = np.zeros(si_t)
+  ei_diab     = np.zeros(si_t)
   
-  for ifi in range(0,nf):
+  for it in range(0,si_t):
     
-    p = read_bas(pfiles[ifi])
-  
+    p = load_generic(pfiles, it, 'p', 1/f0, interp=True, si_t=si_t, subtract_bc=True)
+    if isinstance(forcing_z, list):
+      loc_forcing_z = load_generic(forcing_z, it, 'wekt', f0/dh[0], interp=False, si_t=si_t)
+    if isinstance(forcing_b, list):
+      sst = load_generic(forcing_b, it, 'sst', interp=False, si_t=si_t)
+      wekt = loc_forcing_z*dh[0]/f0 # remove scaling
+      entoc = -0.5*wekt*( sst - toc[0] ) /(toc[0]-toc[1])
+      loc_forcing_b = entoc - np.mean(entoc)
+
     z = laplacian(p,Delta)
     b = p2b(p, dh, f0)
     s = p2stretch(p,dh, N2,f0)
-    w = get_w(p,dh, N2[:,0,0],f0[0,0], Delta, bf,forcing)
+    w = get_w(p,dh, N2[:,0,0],f0[0,0], Delta, bf,loc_forcing_z, forcing_b, nu=(not nu_in_b)*nu, nu4=(not nu_in_b)*nu4)
     q = p2q(p, dh, N2,f0, Delta)
     ke = comp_ke(p,Delta)
     pe = comp_pe(p, dh, N2,f0, Delta)
@@ -310,7 +324,7 @@ def lorenz_cycle(pfiles,dh,N2,f0,Delta,bf=0, nu=0, nu4=0, forcing=0):
     q_p = q - q_me
     ke_p = ke - ke_me
     pe_p = pe - pe_me
-  
+    
     jpz = jacobian(p_p,z_p, Delta)
     jps = jacobian(p_p,s_p, Delta)
   
@@ -324,23 +338,28 @@ def lorenz_cycle(pfiles,dh,N2,f0,Delta,bf=0, nu=0, nu4=0, forcing=0):
 
     bottom_ekman = -bf*laplacian(p_p[-1,:,:],Delta)
   
-    e_surf[0,:,:] = -p_p[0,:,:]*forcing
+    e_surf[0,:,:] = -p_p[0,:,:]*(loc_forcing_z - f_me)
     e_bottom[-1,:,:] = -p_p[-1,:,:]*bottom_ekman
-  
-    ei_ke_me2ke_p[ifi] = integral_plev(ke_me2ke_p, dh, Delta) 
-    ei_pe_me2pe_p[ifi] = integral_plev(pe_me2pe_p, dh, Delta) 
-    ei_surf[ifi]   = integral_plev(e_surf, dh, Delta)
-    ei_bottom[ifi] = integral_plev(e_bottom, dh, Delta)
-    ei_diss_k[ifi] = integral_plev(-p_p*dissip_k, dh, Delta)
-    ei_diss_p[ifi] = integral_plev(-p_p*dissip_p, dh, Delta)
-    ei_wb[ifi]     = integral_blev(w_p*b_p, dh, Delta)
-    ei_ke[ifi]     = integral_plev(ke_p, dh, Delta)
-    ei_pe[ifi]     = integral_blev(pe_p, dh, Delta)
-  
+
+    e_diab[0,:,:] = b_p[0,:,:]*(loc_forcing_b - d_me)
+
+    ei_ke_me2ke_p[it] = integral(ke_me2ke_p, dh, Delta, average)
+    ei_pe_me2pe_p[it] = integral(pe_me2pe_p, dh, Delta, average)
+    ei_surf[it]   = integral(e_surf, dh, Delta, average)
+    ei_bottom[it] = integral(e_bottom, dh, Delta, average)
+    ei_diss_k[it] = integral(-p_p*dissip_k, dh, Delta, average)
+    ei_diss_p[it] = integral(-p_p*dissip_p, dh, Delta, average)
+    ei_wb[it]     = integral(w_p*b_p, dh, Delta, average)
+    ei_ke[it]     = integral(ke_p, dh, Delta, average)
+    ei_pe[it]     = integral(pe_p, dh, Delta, average)
+    ei_diab[it]   = integral(e_diab, dh, Delta, average)
 
   # sign convention matches name
   lec = {}
   lec["f2mke"]   = ei_surf_me             
+  lec["f2eke"]   = np.mean(ei_surf)
+  lec["f2mpe"]   = ei_diab_me             
+  lec["f2epe"]   = np.mean(ei_diab)
   lec["mke2mpe"] = -ei_wb_me              
   lec["epe2eke"] = np.mean(ei_wb)         
   lec["mke2eke"] = np.mean(ei_ke_me2ke_p) 
@@ -356,18 +375,51 @@ def lorenz_cycle(pfiles,dh,N2,f0,Delta,bf=0, nu=0, nu4=0, forcing=0):
   lec["mpe"]     = ei_pe_me      
   lec["epe"]     = np.mean(ei_pe)
 
+  if not nu_in_b:
+    lec["mpe2dis"] = 0
+    lec["epe2dis"] = 0
+  if not isinstance(forcing_z, list):
+    lec["f2eke"]   = 0
+  if not isinstance(forcing_b, list):
+    lec["f2mpe"]   = 0 # not strictly speaking in this if
+    lec["f2epe"]   = 0
+
+  lec["average"] = average
+
   return lec
 
 
-def draw_lorenz_cycle(lec):
+def draw_lorenz_cycle(lec, us=1, ts=1, rho=1):
 
   '''
   Draw Lorenz energy cycle
+
+  To help visualisation, one can use us, ts and rho: 
+  - energies are multiplied by rho/us^2
+  - and fluxes are multiplied by rho*ts/us^2 (amount of energy added/removed in ts)
+
+  if lec["average"] == True, energies from lorenz_cycle are m^5/s^2, fluxes are m^5/s^3
+  
+  When multiplied by rho, energies are in joules and fluxes in watt. You can use
+  us and ts to get multiples of joule and watts 
+  for instance to get energies in EJ (exajoules) and fluxes in GW (gigawatt), set
+  - us = 1e9 (divide energy by 1e18) 
+  - ts = 1e9 (divide flux by 1e9) 
+  
+
+  if lec["average"] == True, energies are m^2/s^2, fluxes are m^2/s^3
+
+  us and ts are just velocity scales and time scales.
+  *no need to set rho in that case*
+
 
   Parameters
   ----------
 
   lec: dict of all energy fluxes and energy reservoirs from lorenz_cycle function
+  us: float, velocity scale factor
+  ts: float, time scale factor
+  rho: float, density
 
   Returns
   -------
@@ -376,21 +428,24 @@ def draw_lorenz_cycle(lec):
 
   '''
 
-  f2mke   = lec["f2mke"]  
-  mke2mpe = lec["mke2mpe"]
-  epe2eke = lec["epe2eke"]
-  mke2eke = lec["mke2eke"]
-  mpe2epe = lec["mpe2epe"]
-  mke2dis = lec["mke2dis"]
-  eke2dis = lec["eke2dis"]
-  mpe2dis = lec["mpe2dis"]
-  epe2dis = lec["epe2dis"]
-  mke2bf  = lec["mke2bf"] 
-  eke2bf  = lec["eke2bf"] 
-  mke     = lec["mke"]    
-  eke     = lec["eke"]    
-  mpe     = lec["mpe"]    
-  epe     = lec["epe"]
+  f2mke   = lec["f2mke"]*rho*ts/us**2  
+  f2eke   = lec["f2eke"]*rho*ts/us**2  
+  f2mpe   = lec["f2mpe"]*rho*ts/us**2
+  f2epe   = lec["f2epe"]*rho*ts/us**2  
+  mke2mpe = lec["mke2mpe"]*rho*ts/us**2
+  epe2eke = lec["epe2eke"]*rho*ts/us**2
+  mke2eke = lec["mke2eke"]*rho*ts/us**2
+  mpe2epe = lec["mpe2epe"]*rho*ts/us**2
+  mke2dis = lec["mke2dis"]*rho*ts/us**2
+  eke2dis = lec["eke2dis"]*rho*ts/us**2
+  mpe2dis = lec["mpe2dis"]*rho*ts/us**2
+  epe2dis = lec["epe2dis"]*rho*ts/us**2
+  mke2bf  = lec["mke2bf"]*rho*ts/us**2 
+  eke2bf  = lec["eke2bf"]*rho*ts/us**2
+  mke     = lec["mke"]*rho/us**2    
+  eke     = lec["eke"]*rho/us**2    
+  mpe     = lec["mpe"]*rho/us**2    
+  epe     = lec["epe"]*rho/us**2
 
   plt.figure()
   plt.text(0.5,0.5  ,"MKE\n {0:0.0f}".format(mke),horizontalalignment='center', verticalalignment='center',bbox=dict(boxstyle="round", fc="w"))
@@ -419,8 +474,23 @@ def draw_lorenz_cycle(lec):
   
   # forcing
   plt.arrow(0.5,1.25,0,-0.5,width=0.01, length_includes_head=True)
-  plt.text(0.55,1, "ws:{0:0.0f}".format(f2mke))
+  plt.text(0.55,1, "W:{0:0.0f}".format(f2mke))
   
+  if f2eke != 0:  
+    f2e_sign = np.sign(f2eke)
+    plt.arrow(0.5,-1.25 + 0.25*(1-f2e_sign),0,f2e_sign*0.5,width=0.01, length_includes_head=True)
+    plt.text(0.55,-1, "W:{0:0.0f}".format(np.abs(f2eke)))
+
+  if f2epe != 0:  
+    f2e_sign = np.sign(f2epe)
+    plt.arrow(-0.5,-1.25 + 0.25*(1-f2e_sign),0,f2e_sign*0.5,width=0.01, length_includes_head=True)
+    plt.text(-0.8,-1, "E:{0:0.0f}".format(np.abs(f2epe)))
+
+  if f2mpe != 0:  
+    f2e_sign = np.sign(f2mpe)
+    plt.arrow(-0.5,1.25 - 0.25*(1-f2e_sign),0,-f2e_sign*0.5,width=0.01, length_includes_head=True)
+    plt.text(-0.8,1, "E:{0:0.0f}".format(np.abs(f2mpe)))
+
   # viscous dissip
   plt.arrow(0.75,0.6,0.5,0,width=0.01, length_includes_head=True)
   plt.text(1,0.65, "D:{0:0.0f}".format(mke2dis),horizontalalignment='center')
@@ -428,11 +498,13 @@ def draw_lorenz_cycle(lec):
   plt.arrow(0.75,-0.4,0.5,0,width=0.01, length_includes_head=True)
   plt.text(1,-0.35, "D:{0:0.0f}".format(eke2dis),horizontalalignment='center')
   
-  plt.arrow(-0.75,0.5,-0.5,0,width=0.01, length_includes_head=True)
-  plt.text(-1,0.55, "D:{0:0.0f}".format(mpe2dis),horizontalalignment='center')
+  if mpe2dis > 0:
+    plt.arrow(-0.75,0.5,-0.5,0,width=0.01, length_includes_head=True)
+    plt.text(-1,0.55, "D:{0:0.0f}".format(mpe2dis),horizontalalignment='center')
   
-  plt.arrow(-0.75,-0.5,-0.5,0,width=0.01, length_includes_head=True)
-  plt.text(-1,-0.45, "D:{0:0.0f}".format(epe2dis),horizontalalignment='center')
+  if epe2dis > 0:
+    plt.arrow(-0.75,-0.5,-0.5,0,width=0.01, length_includes_head=True)
+    plt.text(-1,-0.45, "D:{0:0.0f}".format(epe2dis),horizontalalignment='center')
   
   # Bottom friction
   plt.arrow(0.75,0.4,0.5,0,width=0.01, length_includes_head=True)
