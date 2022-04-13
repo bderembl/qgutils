63917391c9fdc042aea685b91bf0ef442b76c560
bderembl <bruno.deremble@ens.fr>
2022-04-13 12:48:04 +0200

    clean up jacobian

--- qgutils/operators.py
+++ qgutils/operators.py
@@ -6,7 +6,7 @@ import sys
 from .grid import *
 
 
-def jacobian(p, q, Delta, pad=True):
+def jacobian(p, q, Delta, pad='dirichlet'):
   
   """
   Compute Arakawa Jacobian J(p,q) = dp/dx dq/dy - dp/dy dq/dx
@@ -17,44 +17,33 @@ def jacobian(p, q, Delta, pad=True):
   p : array [(nz), ny,nx]
   q   : array [(nz), ny,nx]
   Delta: float
-  pad: bool
+  pad: 'dirichlet', 'neumann', 'periodic', or None
 
   Returns
   -------
 
-  J(p,q): array [(nz), ny,nx]
+  J(p,q): array [(nz), ny+2 ,nx+2] or [(nz), ny ,nx] (if no padding)
   """
 
-
-  nd = p.ndim
-  si = p.shape
-
-  if nd == 1:
-    print("not handeling 1d arrays")
-    sys.exit(1)
-  elif nd == 2: 
-    p = p.reshape(1,si[0],si[1])
-    q   = q.reshape(1,si[0],si[1])
-
   if pad:
-    p = pad_bc(p, bc='dirichlet')
-    q = pad_bc(q, bc='dirichlet')
+    p = pad_bc(p, bc=pad)
+    q = pad_bc(q, bc=pad)
 
 
-  jac = ((q[:,2:,1:-1]-q[:,:-2,1:-1])*(p[:,1:-1,2:]-p[:,1:-1,:-2]) \
-    +(q[:,1:-1 ,:-2]-q[:, 1:-1 ,2:])*(p[:,2:, 1:-1]-p[:,:-2, 1:-1 ]) \
-    + q[:,2:, 1:-1 ]*( p[:,2:,2: ] - p[:,2:,:-2 ]) \
-    - q[:,:-2, 1:-1]*( p[:,:-2,2:] - p[:,:-2,:-2]) \
-    - q[:, 1:-1 ,2:]*( p[:,2:,2: ] - p[:,:-2,2: ]) \
-    + q[:,1:-1 ,:-2]*( p[:,2:,:-2] - p[:,:-2,:-2]) \
-    + p[:, 1:-1 ,2:]*( q[:,2:,2: ] - q[:,:-2,2: ]) \
-    - p[:,1:-1 ,:-2]*( q[:,2:,:-2] - q[:,:-2,:-2]) \
-    - p[:,2:, 1:-1 ]*( q[:,2:,2: ] - q[:,2:,:-2 ]) \
-    + p[:,:-2, 1:-1]*( q[:,:-2,2:] - q[:,:-2,:-2]))\
+  jac = ((q[...,2:,1:-1]-q[...,:-2,1:-1])*(p[...,1:-1,2:]-p[...,1:-1,:-2]) \
+    +(q[...,1:-1 ,:-2]-q[..., 1:-1 ,2:])*(p[...,2:, 1:-1]-p[...,:-2, 1:-1 ]) \
+    + q[...,2:, 1:-1 ]*( p[...,2:,2: ] - p[...,2:,:-2 ]) \
+    - q[...,:-2, 1:-1]*( p[...,:-2,2:] - p[...,:-2,:-2]) \
+    - q[..., 1:-1 ,2:]*( p[...,2:,2: ] - p[...,:-2,2: ]) \
+    + q[...,1:-1 ,:-2]*( p[...,2:,:-2] - p[...,:-2,:-2]) \
+    + p[..., 1:-1 ,2:]*( q[...,2:,2: ] - q[...,:-2,2: ]) \
+    - p[...,1:-1 ,:-2]*( q[...,2:,:-2] - q[...,:-2,:-2]) \
+    - p[...,2:, 1:-1 ]*( q[...,2:,2: ] - q[...,2:,:-2 ]) \
+    + p[...,:-2, 1:-1]*( q[...,:-2,2:] - q[...,:-2,:-2]))\
     /(12.*Delta*Delta)
 
 
-  return jac.squeeze()
+  return jac
 
 
 def interp_on_b(p, dh):
